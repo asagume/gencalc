b698dee266cdfd41463b9f1f7c385d71b61e9ce1
asagume <92704525+asagume@users.noreply.github.com>
2022-07-12 17:36:08 +0900

    h

--- HoYoLAB/GetHoYoWiki.ps1
+++ HoYoLAB/GetHoYoWiki.ps1
@@ -1,6 +1,7 @@
 $destFolder = ".\RawData"
 
 $pageIds = @()
+$pageIds += 30
 $pageIds += 1930
 
 # $xRpcLanguages = @("zh-cn", "zh-tw", "de-de", "en-us", "es-es", "fr-fr", "id-id", "ja-jp", "ko-kr", "pt-pt", "ru-ru", "th-th", "vi-vn")
@@ -22,19 +23,53 @@ foreach ($pageId in $pageIds) {
         Invoke-WebRequest -URI $uri -Headers $headers -OutFile $tempFile
         $rawContent = Get-Content -Path $tempFile -Encoding UTF8
         $contentHashTable = (New-Object System.Web.Script.Serialization.JavaScriptSerializer).Deserialize($rawContent, [System.Collections.HashTable])
-        $contentHashTable = $contentHashTable.data.page
-
-        $contentMLang[$xRpcLanguage] = $contentHashTable
+        if ($contentHashTable.retcode -ne 0) {
+            continue
+        }
+        $contentMLang[$xRpcLanguage] = $contentHashTable.data.page
 
         Remove-Item -Path $tempFile
     } 
 
     $nameEn = $contentMLang["en-us"].name
+    $writableName = $nameEn -replace "[`"<>`|:`*`?`\/ ]", ""
+    $menuId = $contentMLang["en-us"].menu_id
 
-    $menuName = $contentMLang["en-us"].menu_name
-    $menuName
+    $jsonFilePath = ""
 
-    if ($menuName -eq "Weapons") {
+    if ($menuId -eq 2) {
+        # キャラクター
+        foreach ($value in $contentMLang["en-us"]["filter_values"]["character_rarity"]["values"]) {
+            $rarity = $value -replace "[^0-9]", ""
+        }
+        foreach ($value in $contentMLang["en-us"]["filter_values"]["character_vision"]["values"]) {
+            $vision = $value.ToLower()
+        }
+        foreach ($value in $contentMLang["en-us"]["filter_values"]["character_weapon"]["values"]) {
+            $weapon = $value.ToLower()
+        }
+
+        $dirName = $vision + "_" + $rarity + "_" + $weapon + "_" + $writableName
+        $dirPath = Join-Path $destFolder -ChildPath (Join-Path "images\characters" -ChildPath $dirName)
+        if (-not (Test-Path $dirPath)) {
+            New-Item -Path $dirPath -ItemType Directory -Force
+        }
+
+        if ($contentMLang["en-us"]["icon_url"]) {
+            $imageUrl = $contentMLang["en-us"]["icon_url"]
+            $imageFile = $dirName + ".png"
+            $iconDirPath = Join-Path $destFolder -ChildPath ("images\characters\face")
+            if (-not (Test-Path $iconDirPath)) {
+                New-Item -Path $iconDirPath -ItemType Directory -Force
+            }
+            Invoke-WebRequest -URI $imageUrl -Headers $headers -OutFile (Join-Path $iconDirPath -ChildPath $imageFile)
+        }
+
+        if ($contentMLang["en-us"]["header_img_url"]) {
+        }
+    }
+    elseif ($menuId -eq 4) {
+        # 武器
         foreach ($value in $contentMLang["en-us"]["filter_values"]["weapon_type"]["values"]) {
             $type = $value.ToLower()
         }
@@ -42,19 +77,25 @@ foreach ($pageId in $pageIds) {
             $rarity = $value -replace "[^0-9]", ""
         }
 
-        $eliminated = $nameEn -replace "[`"<>`|:`*`?`\/ ]", ""
-
-        $jsonFilePath = $rarity + "_" + $type + "_" + $eliminated + ".json"
-        $jsonFilePath
+        $jsonFilePath = $rarity + "_" + $type + "_" + $writableName + ".json"
+        $jsonFilePath = Join-Path $destFolder -ChildPath $jsonFilePath
 
         if ($contentMLang["en-us"]["icon_url"]) {
             $imageUrl = $contentMLang["en-us"]["icon_url"]
-            $imageFile = $rarity + "_" + $eliminated + ".png"
-            $outDirPath = Join-Path $destFolder -ChildPath ("images\weapon\" + $type)
-            New-Item  -Path $outDirPath -ItemType Directory
-            Invoke-WebRequest -URI $imageUrl -Headers $headers -OutFile (Join-Path $outDirPath -ChildPath $imageFile)
+            $imageFile = $rarity + "_" + $writableName + ".png"
+            $iconDirPath = Join-Path $destFolder -ChildPath ("images\weapons\" + $type)
+            if (-not (Test-Path $iconDirPath)) {
+                New-Item -Path $iconDirPath -ItemType Directory -Force
+            }
+            Invoke-WebRequest -URI $imageUrl -Headers $headers -OutFile (Join-Path $iconDirPath -ChildPath $imageFile)
         }
     }
+    elseif ($menuId -eq 5) {
+        # 聖遺物
+    }
 
+    if ($jsonFilePath -ne "") {
+        $jsonFilePath
+        Out-File -FilePath $jsonFilePath -Encoding utf8 -InputObject (ConvertTo-Json -InputObject $contentMLang["ja-jp"] -Depth 100)
+    }
 }
-
