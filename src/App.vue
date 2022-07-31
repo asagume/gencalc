bc639e7704590b58eb82aea287479937ebdc3fcd
asagume <a.sagume@gmail.com>
2022-07-31 13:35:57 +0900

    vue

--- src/App.vue
+++ src/App.vue
@@ -54,12 +54,9 @@ export default defineComponent({
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 
 .hidden {
