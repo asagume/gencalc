b3dec1a03bc3bf88f98492c5595aa7c76fdf121f
asagume <a.sagume@gmail.com>
2022-08-02 23:53:15 +0900

    vue

---
+++ src/components/ElementalResonanceInput.vue
@@ -0,0 +1,23 @@
+<template></template>
+
+<script lang="ts">
+import GlobalMixin from "@/GlobalMixin.vue";
+import { computed, defineComponent } from "vue";
+
+export default defineComponent({
+  name: "ElementalResonanceInput",
+  mixins: [GlobalMixin],
+  props: {
+    characterInput: { type: Object, require: true },
+    conditionInput: { type: Object, require: true },
+  },
+  setup(props) {
+    const elementalResonanceList = computed(() => ELEMENTAL_RESONANCE_MASTER_LIST);
+
+    return {
+      elementalResonanceList,
+    };
+  },
+});
+</script>
+<style scoped></style>
