1a8185630d0b9d4b0b807a80559a8d5500fc28ae
asagume <92704525+asagume@users.noreply.github.com>
2022-08-19 11:53:26 +0900

    fri

---
+++ temp/deploy.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/sh
+
+# abort on errors
+set -e
+
+NODE_ENV=production
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd docs
+
+cp -f ../public/index2.html index.html
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:asagume/asagume.github.io.git main
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:asagume/gencalc.git main:gh-pages
+
+cd -
