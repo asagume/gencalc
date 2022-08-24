5bee03900bc3957e2c190b2c9a63d2f9aa997110
asagume <92704525+asagume@users.noreply.github.com>
2022-08-24 11:06:12 +0900

    sns

---
+++ temp/ShareSns.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <button type="button" @click="$emit('share:twitter')">
+            <img width="32" height="32" src="images/Twitter social icons - rounded square - blue.png" alt="Twitter">
+        </button>
+    </div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+    name: "ShareSns",
+    emits: ['share:twitter'],
+});
+</script>
+<style scoped>
+button {
+    border: none;
+    background-color: transparent;
+}
+</style>
---
+++ temp/common.ts
@@ -0,0 +1,34 @@
+export function isString(value: any): boolean {
+    return typeof value === 'string' || value instanceof String;
+}
+
+export function isNumber(value: any): boolean {
+    return isFinite(value) && value != null;
+}
+
+export function isPlainObject(value: any): boolean {
+    const myType = Object.prototype.toString.call(value);
+    return myType === '[object Object]';
+}
+
+/** 簡易なdeep copy。undefinedなどは消失します */
+export function deepcopy(value: any): any {
+    const result = JSON.parse(JSON.stringify(value));
+    return result;
+}
+
+export function overwriteObject(dst: any, src: any) {
+    if (isPlainObject(src) && isPlainObject(dst)) {
+        const srcKeys = Object.keys(src);
+        for (const key of srcKeys) {
+            dst[key] = src[key];
+        }
+        for (const key of Object.keys(dst)) {
+            if (!srcKeys.includes(key)) delete dst[key];
+        }
+    }
+    return dst;
+}
+
+// eslint-disable-next-line
+export const basename = (path: string) => path!.split('/')!.pop()!.split('.')!.shift()!;
