9359895d9c91f18fff2a60a6073212324dea6e8f
asagume <92704525+asagume@users.noreply.github.com>
2022-08-29 15:58:48 +0900

    1600

---
+++ temp/CharacterOwnList.vue
@@ -0,0 +1,128 @@
+<template>
+  <ul class="own-list">
+    <li v-for="item in characterList" :key="item.key">
+      <img :class="'character' + bgImageClass(item) + notOwnedClass(item)" :src="item.icon_url" :alt="item.key"
+        @click="onClick(item)" />
+      <div class="tooltip">{{ displayName(item.key) }}</div>
+      <img class="vision" :src="visionSrc(item)" :alt="item.元素" />
+      <div class="constellation">{{ constellationObj[item.key] }}</div>
+    </li>
+  </ul>
+  <label>
+    <input type="checkbox" v-model="savable" :disabled="!changed" />
+    <span>{{ displayName("キャラクター所持状況を保存する") }}</span>
+  </label>
+  <button type="button" @click="save" :disabled="!savable">
+    {{ displayName("実行") }}
+  </button>
+</template>
+
+<script lang="ts">
+import {
+  TCharacterEntry,
+  ELEMENT_IMG_SRC,
+  CHARACTER_MASTER_LIST,
+  STAR_BACKGROUND_IMAGE_CLASS,
+} from "@/master";
+import { defineComponent, reactive, ref } from "vue";
+import CompositionFunction from "./CompositionFunction.vue";
+
+export default defineComponent({
+  name: "CharacterOwnList",
+  setup() {
+    const { displayName } = CompositionFunction();
+
+    const visionSrc = (item: TCharacterEntry) => ELEMENT_IMG_SRC[item.元素];
+    const bgImageClass = (item: TCharacterEntry) =>
+      (" " + STAR_BACKGROUND_IMAGE_CLASS[item.レアリティ]) as string;
+
+    const characterList = CHARACTER_MASTER_LIST;
+    const constellationObj = reactive({} as { [key: string]: number | null });
+    for (const item of characterList) {
+      constellationObj[item.key] = null;
+    }
+    if (localStorage["キャラクター所持状況"]) {
+      const savedObj = JSON.parse(localStorage["キャラクター所持状況"]);
+      Object.keys(savedObj).forEach((key) => {
+        constellationObj[key] = savedObj[key];
+      });
+    }
+    const changed = ref(false);
+    const savable = ref(false);
+
+    const notOwnedClass = (item: TCharacterEntry) =>
+      constellationObj[item.key] == null ? " not-owned" : "";
+
+    const onClick = (item: TCharacterEntry) => {
+      if (constellationObj[item.key] === null) {
+        constellationObj[item.key] = 0;
+      } else if (constellationObj[item.key] == 6) {
+        constellationObj[item.key] = null;
+      } else {
+        let current = constellationObj[item.key] as number;
+        constellationObj[item.key] = current + 1;
+      }
+      changed.value = true;
+    };
+
+    const save = () => {
+      const saveObj = {} as { [key: string]: number | null };
+      Object.keys(constellationObj)
+        .filter((s) => constellationObj[s] !== null)
+        .forEach((key) => {
+          saveObj[key] = constellationObj[key];
+        });
+      localStorage.setItem("キャラクター所持状況", JSON.stringify(saveObj));
+      savable.value = false;
+      changed.value = false;
+    };
+
+    return {
+      displayName,
+
+      visionSrc,
+      bgImageClass,
+      characterList,
+      constellationObj,
+      onClick,
+      changed,
+      savable,
+      save,
+      notOwnedClass,
+    };
+  },
+});
+</script>
+
+<style scoped>
+img.character {
+  width: 90px;
+  height: 90px;
+  background-size: contain;
+}
+
+img.vision {
+  width: 30px;
+  height: 30px;
+  position: absolute;
+  left: 3px;
+  top: 3px;
+}
+
+div.constellation {
+  font-size: 28px;
+  text-shadow: 0 0 3px black;
+  position: absolute;
+  right: 10px;
+  top: 3px;
+}
+
+.not-owned {
+  opacity: 50%;
+}
+
+.tooltip {
+  top: unset;
+  bottom: 0.5rem;
+}
+</style>
---
+++ temp/WeaponOwnList.vue
@@ -0,0 +1,154 @@
+<template>
+  <ul class="select-list">
+    <li v-for="item in weaponTypeList" :key="item">
+      <label>
+        <input class="hidden" type="radio" v-model="selectedWeaponType" name="weapon-own-list-type" :value="item" />
+        <img class="filter" :src="weaponSrc(item)" :alt="item" />
+      </label>
+    </li>
+  </ul>
+  <div v-for="weaponType in weaponTypeList" :key="weaponType">
+    <ul class="own-list" v-if="weaponType == selectedWeaponType">
+      <li v-for="item in weaponList(weaponType)" :key="item.key">
+        <img :class="'weapon' + bgImageClass(item) + notOwnedClass(item)" :src="item.icon_url" :alt="item.key"
+          @click="onClick(item)" />
+        <div class="tooltip">{{ displayName(item.key) }}</div>
+        <div class="refine">{{ refineObj[item.key] }}</div>
+      </li>
+    </ul>
+  </div>
+  <label>
+    <input type="checkbox" v-model="savable" :disabled="!changed" />
+    <span>{{ displayName("武器所持状況を保存する") }}</span>
+  </label>
+  <button type="button" @click="save" :disabled="!savable">
+    {{ displayName("実行") }}
+  </button>
+</template>
+
+<script lang="ts">
+import {
+  STAR_BACKGROUND_IMAGE_CLASS,
+  TWeaponEntry,
+  WEAPON_MASTER_LIST,
+  TWeaponTypeKey,
+  WEAPON_IMG_SRC,
+} from "@/master";
+import { computed, defineComponent, reactive, ref } from "vue";
+import CompositionFunction from "./CompositionFunction.vue";
+
+export default defineComponent({
+  name: "WeaponOwnList",
+  setup() {
+    const { displayName } = CompositionFunction();
+
+    const bgImageClass = (item: TWeaponEntry) =>
+      (" " + STAR_BACKGROUND_IMAGE_CLASS[item.レアリティ]) as string;
+
+    const weaponTypeList = computed(
+      () => Object.keys(WEAPON_IMG_SRC) as TWeaponTypeKey[]
+    );
+    const weaponSrc = (weapon: TWeaponTypeKey) => WEAPON_IMG_SRC[weapon] as string;
+    const weaponList = (weaponType: TWeaponTypeKey) =>
+      WEAPON_MASTER_LIST[weaponType] as any[];
+    const selectedWeaponType = ref(weaponTypeList.value[0]);
+
+    const refineObj = reactive({} as { [key: string]: number | null });
+    for (const weaponType of weaponTypeList.value) {
+      for (const item of weaponList(weaponType)) {
+        refineObj[item.key] = null;
+      }
+    }
+    if (localStorage["武器所持状況"]) {
+      const savedObj = JSON.parse(localStorage["武器所持状況"]);
+      Object.keys(savedObj).forEach((key) => {
+        refineObj[key] = savedObj[key];
+      });
+    }
+    const changed = ref(false);
+    const savable = ref(false);
+
+    const notOwnedClass = (item: TWeaponEntry) =>
+      refineObj[item.key] == null ? " not-owned" : "";
+
+    const onClick = (item: TWeaponEntry) => {
+      if (refineObj[item.key] === null) {
+        refineObj[item.key] = 1;
+      } else if (refineObj[item.key] == 5) {
+        refineObj[item.key] = null;
+      } else {
+        let current = refineObj[item.key] as number;
+        refineObj[item.key] = current + 1;
+      }
+      changed.value = true;
+    };
+
+    const save = () => {
+      const saveObj = {} as { [key: string]: number | null };
+      Object.keys(refineObj)
+        .filter((s) => refineObj[s] !== null)
+        .forEach((key) => {
+          saveObj[key] = Number(refineObj[key]);
+        });
+      localStorage.setItem("武器所持状況", JSON.stringify(saveObj));
+      savable.value = false;
+      changed.value = false;
+    };
+
+    return {
+      displayName,
+
+      bgImageClass,
+      weaponTypeList,
+      weaponSrc,
+      weaponList,
+      selectedWeaponType,
+      refineObj,
+      onClick,
+      changed,
+      savable,
+      save,
+      notOwnedClass,
+    };
+  },
+});
+</script>
+
+<style scoped>
+img.weapon {
+  width: 90px;
+  height: 90px;
+  background-size: contain;
+}
+
+.weapon.selected {
+  background-color: gold;
+}
+
+img.filter {
+  width: 45px;
+  height: 45px;
+  border-radius: 50%;
+}
+
+:checked+img {
+  background-color: rgb(156, 140, 49);
+}
+
+div.refine {
+  font-size: 28px;
+  text-shadow: 0 0 3px black;
+  position: absolute;
+  right: 10px;
+  top: 3px;
+}
+
+.not-owned {
+  opacity: 50%;
+}
+
+.tooltip {
+  top: unset;
+  bottom: 0.5rem;
+}
+</style>
