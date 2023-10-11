ff73134b0f24f64f7d678eccdf1a56bf6f0c64bc
asagume <a.sagume@gmail.com>
2023-10-11 20:27:18 +0900

    TeamManager

--- src/pages/TeamManager/TeamRotation.vue
+++ src/pages/TeamManager/TeamRotation.vue
@@ -58,7 +58,7 @@
                 <div v-if="removeMode" class="remove-mark" @click="removeItemOnClick(element)"></div>
               </div>
               <div class="action-attribute">
-                {{ actionAttribute(element) }}
+                {{ actionDisplay(element) }}
               </div>
             </div>
           </div>
@@ -68,16 +68,15 @@
       <textarea class="rotation-description" v-model="rotationDescription" rows="10" maxlength="400"
         @change="updateRotation"></textarea>
     </fieldset>
-    <!-- {{ memberParticles }}
-    {{ memberEnergyRecharge }} -->
   </div>
 </template>
 <script lang="ts">
 import draggable from "vuedraggable";
-import { computed, defineComponent, onMounted, PropType, reactive, ref, watch } from "vue";
+import { defineComponent, onMounted, PropType, reactive, ref, watch } from "vue";
 import CompositionFunction from "@/components/CompositionFunction.vue";
 import { TActionItem, TMember, TTeam } from "./team";
-import { CHARACTER_MASTER, ELEMENT_BG_COLOR_CLASS, ELEMENT_COLOR_CLASS, getCharacterMasterDetail, IMG_SRC_DUMMY, TAnyObject, TCharacterDetail, TCharacterEntry, TCharacterKey } from "@/master";
+import { CHARACTER_MASTER, ELEMENT_BG_COLOR_CLASS, ELEMENT_COLOR_CLASS, getCharacterMasterDetail, IMG_SRC_DUMMY, TCharacterDetail, TCharacterEntry, TCharacterKey } from "@/master";
+import _ from "lodash";
 
 export default defineComponent({
   name: "TeamRotation",
@@ -94,10 +93,18 @@ export default defineComponent({
     const NORMAL_ATTACK_ACTION_LIST = ['N', 'C', 'P']; // 通常攻撃, 重撃, 落下攻撃
     const characterDetailMap = new Map();
     const normalAttackDanMap = new Map(); // 通常攻撃の段数
-    const elementalSkillActionsMap = new Map(); // 元素スキル 一回押し 長押し
+    const elementalSkillActionsMap = new Map<string, string[]>(); // 元素スキル 一回押し 長押し
     const rotationList = reactive([] as TActionItem[]);
     const actionId = ref(0);
     const rotationDescription = ref('');
+
+    // 重撃の前に通常が挟まる武器、キャラクター
+    const CHARGED_WITH_NORMAL_WEAPON = ['片手剣', '長柄武器'];
+    const CHARGED_WITH_NORMAL_CHARACTER = ['鹿野院平蔵', 'リオセスリ'];
+
+    const CT_KEY = 'クールタイム';
+    const E_PRESS_CT_KEY = ['一回押しクールタイム', '基本クールタイム'];
+    const E_HOLD_CT_KEY = ['長押しクールタイム', '最大チャージクールタイム', '1段チャージクールタイム', '2段チャージクールタイム'];
 
     const watchCount = ref(0);
     watch(props, async () => {
@@ -118,7 +125,16 @@ export default defineComponent({
           }
         }
         if (team.rotation && team.rotation.length) {
-          rotationList.splice(0, rotationList.length, ...team.rotation);
+          const workArr = _.cloneDeep(team.rotation);
+          workArr.forEach(e => {
+            if (e.action.startsWith('E')) {
+              const actions = elementalSkillActionsMap.get(e.member);
+              if (actions && actions.length > 0 && !actions.includes(e.action)) {
+                e.action = actions[0];
+              }
+            }
+          })
+          rotationList.splice(0, rotationList.length, ...workArr);
           actionId.value = Math.max(...rotationList.map(s => s.id));
         } else {
           rotationList.splice(0, rotationList.length);
@@ -168,34 +184,34 @@ export default defineComponent({
 
     function getElementalSkillActions(name: string) {
       const result = [];
+      let hasPressHold = false;
       const master = getCharacterDetail(name);
-      if (master && master?.元素スキル?.説明) {
-        const description = master?.元素スキル?.説明;
-        if (description.indexOf('>一回押し<') !== -1 && description.indexOf('>長押し<') !== -1) {
-          result.push('E.Press');
-          result.push('E.Hold');
+      if (master?.元素スキル) {
+        Object.keys(master.元素スキル).filter(key => key.endsWith(CT_KEY)).forEach(key => {
+          if (hasPressHold) return;
+          if (E_PRESS_CT_KEY.includes(key) || E_HOLD_CT_KEY.includes(key)) {
+            hasPressHold = true;
+          } else {
+            hasPressHold = !_.isNumber(master.元素スキル[key]);
+          }
+        })
+        if (!hasPressHold) {
+          const description = master.元素スキル.説明;
+          hasPressHold = description.indexOf('>一回押し<') !== -1 || description.indexOf('>長押し<') !== -1;
         }
-        if (result.length == 0) {
-          result.push('E');
-        }
+      }
+      if (hasPressHold) {
+        result.push('E.Press');
+        result.push('E.Hold');
+      } else {
+        result.push('E');
       }
       return result;
     }
 
-    const getNormalAttackDetail = (member: TMember) => {
-      const master = getCharacterDetail(member.name);
-      return master ? master.通常攻撃 : undefined;
-    };
-
-    const getElementalSkillDetail = (member: TMember) => {
-      const master = getCharacterDetail(member.name);
-      return master ? master.元素スキル : undefined;
-    };
-
-    const getElementalBurstDetail = (member: TMember) => {
-      const master = getCharacterDetail(member.name);
-      return master ? master.元素爆発 : undefined;
-    };
+    const getNormalAttackDetail = (member: TMember) => getCharacterDetail(member.name)?.通常攻撃;
+    const getElementalSkillDetail = (member: TMember) => getCharacterDetail(member.name)?.元素スキル;
+    const getElementalBurstDetail = (member: TMember) => getCharacterDetail(member.name)?.元素爆発;
 
     const getActionDetail = (item: TActionItem) => {
       let result;
@@ -237,75 +253,8 @@ export default defineComponent({
       return NORMAL_ATTACK_ACTION_LIST.includes(action) || action.startsWith('N');
     }
 
-    const actionAttribute = (item: TActionItem) => {
-      let result = '';
-      if (isActionNormalAttack(item.action)) { // 通常攻撃, 重撃, 落下攻撃
-        result = item.action;
-      } else if (item.action.startsWith('E')) { // 元素スキル
-        result = item.action.replace(/^E\./, '');
-      } else if (item.action == 'Q') { // 元素爆発
-        //const master = getCharacterDetail(item.member);
-        //result = 'Q' + master?.元素爆発?.元素エネルギー ?? '';
-        result = 'Q';
-      }
-      return result;
-    }
-
-    // 初期粒子数, 継続粒子数, CT, 継続時間
-    function getParticleInfo(name: string): [string, number, number, number, number] {
-      let result: [string, number, number, number, number] = ['', 0, 0, 0, 0];
-      const master = getCharacterDetail(name);
-      if (master) {
-        result = [(master.元素 as string), 3, 0, 0, 0];
-      } else {
-        result = ['白', 3, 0, 0, 0];
-      }
-      return result;
-    }
-
-    const memberParticles = computed(() => {
-      const result: TAnyObject = {};
-      for (let i = 0; i < rotationList.length; i++) {
-        const rotation = rotationList[i];
-        if (rotation.action.startsWith('E')) {
-          const info = getParticleInfo(rotation.member);
-          if (info[1]) {
-            const nextRotation = (i + 1) < rotationList.length ? rotationList[i + 1] : rotationList[0];
-            if (!(nextRotation.member in result)) {
-              result[nextRotation.member] = {};
-            }
-            const color = info[0];
-            const currentNum = result[nextRotation.member][color] ?? 0;
-            result[nextRotation.member][color] = currentNum + info[1];
-          }
-        }
-      }
-      return result;
-    })
-
-    const memberEnergyRecharge = computed(() => {
-      const result: TAnyObject = {};
-      props.team.members.forEach(member => {
-        let energy = 0;
-        const master = getCharacterDetail(member.name);
-        Object.keys(memberParticles.value).forEach(name2 => {
-          Object.keys(memberParticles.value[name2]).forEach(color => {
-            let work = memberParticles.value[name2][color];
-            if (color === '白') {
-              work *= 2;
-            } else if (color == master?.元素) {
-              work *= 3;
-            }
-            if (member.name != name2) {
-              work *= 0.6;
-            }
-            energy += work;
-          })
-        })
-        result[member.name] = energy;
-      })
-      return result;
-    })
+    const actionDisplay = (item: TActionItem) =>
+      item.action.startsWith('E') ? item.action.replace(/^E\./, '') : item.action;
 
     const updateRotation = () => {
       context.emit('update:rotation', rotationList, rotationDescription.value);
@@ -327,7 +276,10 @@ export default defineComponent({
         if (workArr.length > 0) {
           action = workArr[workArr.length - 1].action;
         } else {
-          action = elementalSkillActionsMap.get(member.name)[0];
+          const actions = elementalSkillActionsMap.get(member.name);
+          if (actions && actions.length > 0) {
+            action = actions[0];
+          }
         }
       }
       rotationList.push({
@@ -349,7 +301,7 @@ export default defineComponent({
           const work = item.action.substring(1, 2);
           let n = work ? Number(work) : 1;
           const dan = normalAttackDanMap.get(item.member) ?? 1;
-          if (master && ['片手剣', '長柄武器'].includes(master.武器)) {
+          if (master && (CHARGED_WITH_NORMAL_WEAPON.includes(master.武器) || CHARGED_WITH_NORMAL_CHARACTER.includes(item.member))) {
             if (item.action.length < 3) {
               item.action = 'N' + n + 'C';
             } else {
@@ -365,7 +317,7 @@ export default defineComponent({
         }
       } else if (item.action.startsWith('E')) { // 元素スキル
         const actions = elementalSkillActionsMap.get(item.member);
-        if (actions.length > 1) {
+        if (actions && actions.length > 1) {
           let index = actions.indexOf(item.action);
           index = index < (actions.length - 1) ? index + 1 : 0;
           item.action = actions[index];
@@ -388,7 +340,6 @@ export default defineComponent({
       bgColorClass,
 
       getCharacterMaster,
-
       getNormalAttackDetail,
       getElementalSkillDetail,
       getElementalBurstDetail,
@@ -398,9 +349,7 @@ export default defineComponent({
       previousRotation,
       getCharacterDetail,
       getActionDetail,
-      actionAttribute,
-      memberParticles,
-      memberEnergyRecharge,
+      actionDisplay,
 
       rotationListOnChange,
       listActionOnClick,
